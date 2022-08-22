6da0deb65f1088fe9ddac266196495dac4e529eb
HeilMartinos <marcinbator.ofc@gmail.com>
2022-08-22 12:52:31 +0200

    better gallery

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
+var whole = document.getElementById("exit");
+function hide() {
+  for (i = 1; i <= 4; i++) {
+    var showed = document.getElementById("photo" + i + "a");
+    showed.classList.replace("visible", "image2");
+  }
+  var exit = document.getElementById("exit");
+  exit.classList.replace("visiblex", "invisiblex");
+}
+whole.addEventListener("click", hide);
 function show(number) {
   var showed = document.getElementById("photo" + number + "a");
   showed.classList.replace("image2", "visible");
   var exit = document.getElementById("exit");
   exit.classList.replace("invisiblex", "visiblex");
-}
-function hide() {
-  for (i = 1; i <= 4; i++) {
-    var showed = document.getElementById("photo" + i + "a");
-    showed.classList.replace("visible", "image2");
-
-    console.log("xd");
-  }
-  var exit = document.getElementById("exit");
-  exit.classList.replace("visiblex", "invisiblex");
 }
 var photo1 = document.getElementById("photo1");
 photo1.addEventListener("click", function () {
@@ -30,5 +30,3 @@ var photo4 = document.getElementById("photo4");
 photo4.addEventListener("click", function () {
   show(4);
 });
-var whole = document.getElementById("exit");
-whole.addEventListener("click", hide);
